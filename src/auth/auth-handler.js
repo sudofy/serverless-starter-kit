27ed94a4dacef65576ccb8f1ca817b5546ef3b3e
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2019-01-10 17:39:04 +0500

    fix imports.

--- src/auth/auth-handler.js
+++ src/auth/auth-handler.js
@@ -2,7 +2,7 @@ import { parseAPIGatewayEvent } from 'utils/parser';
 import { success, failure } from 'utils/response';
 import { checkSignUpSchema, checkSignInSchema, checkForgotPasswordSchema, checkVerifyPinSchema } from 'utils/validation';
 import Boom from 'boom';
-import connectToDatabase from 'utils/db-connection';
+import connectToDatabase from '../utils/db.connection';
 import AuthController from './auth.controller';
 
 const auth = new AuthController();
