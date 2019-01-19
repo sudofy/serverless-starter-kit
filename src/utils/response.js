571a01c1e142dbac4a3d9b4bf4381f79dea90f45
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2019-01-19 11:05:52 +0500

    Add raven configurations.

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,11 +1,10 @@
 import boom from 'boom';
 import serializeError from 'serialize-error';
-// import Raven from 'raven';
-// import log from 'utils/log';
+import Raven from 'raven';
 
-// Raven.config(process.env.RAVEN_CONFIG, {
-//   release: '1.3.0'
-// }).install();
+Raven.config(process.env.RAVEN_CONFIG, {
+  release: '1.3.0'
+}).install();
 
 function buildResponse(statusCode, data, message, isSuccess, noCache) {
   const body = JSON.stringify({
@@ -35,37 +34,30 @@ export function success(body, isCreated, message, noCache) {
 
 function reportToRaven(err, event) {
   if (event && event.requestContext && event.requestContext.authorizer && event.requestContext.authorizer.claims) {
-    delete event.requestContext.authorizer.claims['custom:pincode'];
-    // Raven.setContext({
-    //   user: event.requestContext.authorizer.claims
-    // });
+    Raven.setContext({
+      user: event.requestContext.authorizer.claims
+    });
   }
   return new Promise((resolve) => {
     resolve();
-    // Raven.captureException(err, { extra: { err, event } }, (sendErr, eventId) => {
-    //   if (sendErr) {
-    //     // console.log('Raven Report Error');
-    //     // console.log(sendErr);
-    //   } else {
-    //     // console.log(`Raven Report id ${eventId}`);
-    //   }
-    //   resolve();
-    // });
+    Raven.captureException(err, { extra: { err, event } }, (sendErr, eventId) => {
+      if (sendErr) {
+        console.log('Raven Report Error');
+        console.log(sendErr);
+      } else {
+        console.log(`Raven Report id ${eventId}`);
+      }
+      resolve();
+    });
   });
 }
 
 export async function failure(err, event) {
   await reportToRaven(err, event);
   if (boom.isBoom(err)) {
-    // console.log('BOOM ERROR');
-    // console.log(err);
     return buildResponse(err.output.statusCode, err, err.data ? err.data.message : err.output.payload.message, false);
   }
-  // console.log('JS ERROR');
-  // console.log(err);
   const serializedErr = serializeError(err);
-  // console.log('JS serialized ERROR');
-  // console.log(serializedErr);
   let errMessage;
   if (serializedErr && serializedErr.message) {
     errMessage = serializedErr.message;
